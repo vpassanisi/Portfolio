e9a35dd620b5a1a1e568a4b4885d19e4e062c622
vpassanisi <vpassanisi@gmail.com>
2020-05-11 17:39:21 -0700

    styling

--- src/Snippets.svelte
+++ src/Snippets.svelte
@@ -7,20 +7,20 @@
     class="flex flex-col items-center justify-center text-white text-6xl
     font-hairline tracking-wider mt-8">
     Code Snippets
-    <hr class="w-1/3 text-white mb-4" />
+    <!-- <hr class="w-1/3 text-white mb-4" /> -->
   </div>
   <div class="flex justify-center">
     <div class="flex flex-col max-w-screen-xl border p-4 w-full h-full mb-8">
 
       <div class="text-white text-4xl font-hairline">Javascript</div>
-      <div>
+      <div class="mb-8">
         {#each javascriptSnippets as snippet, i}
           <a
             href={snippet.link}
             target="_blank"
-            class="flex flex-row text-white w-full sm:w-1/2 py-2 px-4
-            bg-gray-900 hover:bg-yellow-400 hover:text-black transitions-colors
-            duration-500 ease-in-put ml-4 m-2">
+            class="flex flex-row text-white sm:w-1/2 py-4 px-6 bg-gray-900
+            hover:bg-yellow-400 hover:text-black transitions-colors duration-500
+            ease-in-put my-2 mx-4">
             {snippet.title}
           </a>
         {/each}
@@ -32,9 +32,9 @@
           <a
             href={snippet.link}
             target="_blank"
-            class="flex flex-row text-white w-full sm:w-1/2 p-2 bg-gray-900
+            class="flex flex-row text-white sm:w-1/2 py-4 px-6 bg-gray-900
             hover:bg-light-blue-300 transitions-colors duration-500 ease-in-put
-            ml-4 m-2">
+            my-2 mx-4">
             {snippet.title}
           </a>
         {/each}
